ea3a02eb65626862a2a7c72c9d5baafff062bf0f
Azreil-OFD <fantom2413@gmail.com>
2024-05-29 21:37:22 +0500

    initial

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>{{ data }}</div>
+  <div>{{ error }}</div>
+</template>
+
+<script lang="ts" setup>
+const { data, pending, error, refresh } = useFetch("/api/hello");
+</script>
+
+<style></style>
