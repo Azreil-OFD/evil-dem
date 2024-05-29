e2b1ba469b149e23d9db57c8d586416cd1ce6c5b
Azreil-OFD <fantom2413@gmail.com>
2024-05-29 21:58:59 +0500

    add eslint

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>{{ data }}</div>
-  <div>{{ error }}</div>
+    <button @click="load">load</button>
+
+    <div>{{ data }}</div>
+    <div>{{ error }}</div>
 </template>
 
 <script lang="ts" setup>
-const { data, pending, error, refresh } = useFetch("/api/hello");
+const data: any = ref(null);
+const error: any = ref(null);
+
+const load = () => {
+    const result = useFetch('/api/hello');
+    data.value = result.data;
+    error.value = result.error;
+};
 </script>
 
 <style></style>
