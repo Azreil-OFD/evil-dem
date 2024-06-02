be8f4d49baebc9967582059d764b217b6a2830ce
Azreil-OFD <fantom2413@gmail.com>
2024-06-02 05:07:06 +0500

    fix signup.ts

--- server/api/auth/signup.ts
+++ server/api/auth/signup.ts
@@ -1,4 +1,3 @@
-import { PrismaClient } from '@prisma/client';
 import SignUp from '~/utils/server/SignUp';
 import generateAccessToken from '~/utils/server/generateAccessToken';
 import generateRefreshToken from '~/utils/server/generateRefreshToken';
@@ -19,21 +18,31 @@ export default defineEventHandler(async (event) => {
         };
     }
     const createUserResult = await SignUp(body);
-    if (createUserResult.status) {
-        return {
-            data: {
-                token: generateAccessToken(createUserResult.user),
-                refresh: generateRefreshToken(createUserResult.user),
-            },
-            error: null,
-        };
-    } else {
-        return {
-            data: null,
-            error: {
-                code: 409,
-                message: createUserResult.error,
-            },
-        };
+    if (createUserResult) {
+        if (createUserResult.status) {
+            if (createUserResult.user)
+                return {
+                    data: {
+                        token: generateAccessToken(createUserResult.user),
+                        refresh: generateRefreshToken(createUserResult.user),
+                    },
+                    error: null,
+                };
+        } else {
+            return {
+                data: null,
+                error: {
+                    code: 409,
+                    message: createUserResult.error,
+                },
+            };
+        }
     }
+    return {
+        data: null,
+        error: {
+            code: 500,
+            message: 'Internal Server Error',
+        },
+    };
 });
--- server/api/hello.ts
+++
@@ -1,3 +0,0 @@
-export default defineEventHandler(async (event) => {
-    return process.env.NUXT_AUTH_TOKEN_SALT;
-});
